7966a38e6701fc19889c86e6a74fa03d94d5f747
farjad1205 <farjad1205@gmail.com>
2025-10-26 02:03:40 -0500

    players can pick their mark and class, but class needs to be implemented

--- game.cpp
+++ game.cpp
@@ -5,6 +5,35 @@
 #include <cctype>
 using namespace std;
 
+bool validate(string type, string input)
+{
+    if (type == "gamemode")
+        if (input == "original" || input == "battle")
+            return true;
+
+    if (type == "class")
+        if (input == "paladin" || input == "chronomage" || input == "alchemist")
+            return true;
+
+    return false;
+}
+
+string start_menu()
+{
+    cout << "Welcome to Tic Tac Toe" << endl;
+    cout << "Would you like to play original or battle mode? (please type exactly \"original\" or \"battle\")" << endl;
+    string mode;
+    cin >> mode;
+    while (!validate("gamemode", mode))
+    {
+        cin.clear();
+        cout << "please type exactly \"original\" or \"battle\" to proceed" << endl;
+        cin >> mode;
+    }
+
+    return mode;
+}
+
 void print_board(char board[3][3])
 {
     char display[5][5] = {
@@ -25,9 +54,9 @@ void print_board(char board[3][3])
     return;
 }
 
-void build_board(char board[3][3])
+void intro_board(char board[3][3])
 {
-    cout << "Welcome to tic tac toe!" << endl;
+    cout << endl;
     cout << "Below is the locations of each square." << endl;
     cout << "If you would like to place your move in that location, enter that number on your turn." << endl;
 
@@ -76,11 +105,11 @@ bool check_loc(char board[3][3], int marker)
         return true;
 }
 
-void player_move(char board[3][3], int count)
+void player_move(char board[3][3], int count, char p1, char p2)
 {
     if (count % 2 == 0)
     {
-        cout << "player X enter a location to mark " << endl;
+        cout << "player 1 enter a location to mark " << endl;
         int turn;
         cin >> turn;
         while (!check_loc(board, turn))
@@ -93,16 +122,16 @@ void player_move(char board[3][3], int count)
 
         int r = (turn - 1) / 3;
         int c = (turn - 1) % 3;
-        board[r][c] = 'X';
+        board[r][c] = p1;
         cout << "turn sucessfully placed" << endl;
         print_board(board);
         check_win(board);
         if (check_win(board))
-            cout << "player X wins" << endl;
+            cout << "player 1 wins" << endl;
     }
     else
     {
-        cout << "player O enter a location to mark " << endl;
+        cout << "player 2 enter a location to mark " << endl;
         int turn;
         cin >> turn;
         while (!check_loc(board, turn))
@@ -115,86 +144,126 @@ void player_move(char board[3][3], int count)
 
         int r = (turn - 1) / 3;
         int c = (turn - 1) % 3;
-        board[r][c] = 'O';
+        board[r][c] = p2;
         cout << "turn sucessfully placed" << endl;
         print_board(board);
         check_win(board);
         if (check_win(board))
-            cout << "player O wins" << endl;
+            cout << "player 2 wins" << endl;
     }
 }
 
-bool validate(string type, string input)
-{
-    if (type == "mode")
-    {
-        // for when the user has to input and gamemode
-        // transform(input.begin(), input.end(), input.begin(), [](unsigned char c)
-        //{return tolower(c);});
-        if (input == "original" || input == "battle")
-        {
-            return true;
-        }
-    }
-    if (type == "mark")
-    {
-        // for when the use has to pick a marker
-    }
-    return false;
-}
-
+char p1, p2;
 void char_select()
 {
-    cout << "In battle mode, you can choose your own marker (still must be only 1 character)" << endl;
+    cout << "In battle mode, you can choose your own marker (only the first character entered will be used)" << endl;
+    cout << "player 1 enter your mark: ";
+    char mark1;
+    cin >> p1;
+    cout << "player 2 enter your mark: ";
+    char mark2;
+    cin >> p2;
+    cout << endl;
 }
 
-void start_menu()
+string c1, c2;
+void class_select()
 {
-    cout << "Welcome to Tic Tac Toe" << endl;
-    cout << "Would you like to play original or battle mode? (please type exactly \"original\" or \"battle\")" << endl;
-    string mode;
-    cin >> mode;
-    while (!validate("mode", mode))
+    cout << "player 1 please choose one of the following class types:" << endl;
+    cout << " “paladin”, “alchemist”, or “chronomage” (must be spelled exactly)" << endl;
+    cin >> c1;
+    while (!validate("class", c1))
     {
+        cout << "check your spelling and try again" << endl;
         cin.clear();
-        cout << "please enter your choice of game mode" << endl;
-        cin >> mode;
+        cin >> c1;
+    }
+    cout << "player1 (" << p1 << ") is " << c1 << endl;
+    cin.clear();
+
+    cout << "player 2 please choose one of the following class types:" << endl;
+    cout << " “paladin”, “alchemist”, or “chronomage” (must be spelled exactly)" << endl;
+    cin >> c2;
+    while (!validate("class", c2))
+    {
+        cout << "check your spelling and try again" << endl;
+        cin.clear();
+        cin >> c2;
     }
 
-    if (mode == "battle")
+    cout << "player2 (" << p2 << ") is " << c1 << endl;
+}
+
+void run_game()
+{
+    char start_board[3][3];
+    intro_board(start_board);
+    print_board(start_board);
+    int count = 0;
+    cout << endl;
+
+    char board[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}};
+
+    print_board(board);
+
+    while (!check_win(board))
     {
-        char_select();
+        player_move(board, count, 'X', 'O');
+        count++;
+        if (count == 10)
+        {
+            cout << "draw" << endl;
+            break;
+        }
+    }
+}
+
+void run_battle(char p1, char p2, string c1, string c2)
+{
+    cout << p1 << p2 << endl;
+    char start_board[3][3];
+    intro_board(start_board);
+    print_board(start_board);
+    int count = 0;
+    cout << endl;
+
+    char board[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}};
+
+    print_board(board);
+
+    while (!check_win(board))
+    {
+        player_move(board, count, p1, p2);
+        count++;
+        if (count == 10)
+        {
+            cout << "draw" << endl;
+            break;
+        }
     }
 }
 
 int main()
 {
-    start_menu();
+    string mode = start_menu();
 
-    // char start_board[3][3];
-    // build_board(start_board);
-    // print_board(start_board);
-    // int count = 0;
-    // cout << endl;
+    if (mode == "original")
+    {
+        cout << "player 1 is X and player 2 is O" << endl;
+        run_game();
+    }
+    else if (mode == "battle")
+    {
+        char_select();
+        class_select();
+        run_battle(p1, p2, c1, c2);
+    }
 
-    // char board[3][3] = {
-    //     {' ', ' ', ' '},
-    //     {' ', ' ', ' '},
-    //     {' ', ' ', ' '}};
-
-    // print_board(board);
-
-    // cout << check_win(board) << endl;
-
-    // while (!check_win(board))
-    // {
-    //     player_move(board, count);
-    //     count++;
-    //     if (count == 10)
-    //     {
-    //         cout << "draw" << endl;
-    //         break;
-    //     }
-    // }
     // cout << "thanks for playing";
 }
